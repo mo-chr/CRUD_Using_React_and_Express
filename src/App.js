28aa5b041faf6a6780773906b3c8adf4684c23d3
Mohammad Chreif <cmohamadch@gmail.com>
2022-11-08 11:37:38 +0200

    CRUD using react and Express

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [name, setName] = useState("");
+  const [dob, setDob] = useState("");
+  const [country, setCountry] = useState("");
+  const [wage, setWage] = useState(0);
+  const [position, setPosition] = useState("");
+
+  const displayInfo = () => {
+    console.log(name + " " + dob + " " + country + " " + wage + " " + position);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Information">
+        <label> Name </label>
+        <input
+          type="text"
+          name="Name"
+          id="name"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+
+        <label>Date of Birth</label>
+        <input
+          type="date"
+          name="Dob"
+          id="dob"
+          onChange={(event) => {
+            setDob(event.target.value);
+          }}
+        />
+
+        <label> Country</label>
+        <input
+          type="text"
+          name="Country"
+          id="country"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        />
+
+        <label> Wage(Monthly in $)</label>
+        <input
+          type="number"
+          name="Wage"
+          id="wage"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        />
+
+        <label>Position</label>
+        <input
+          type="text"
+          name="Position"
+          id="position"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        />
+
+        <button className="submitBtn" onClick={displayInfo}>
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
